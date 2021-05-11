32f4080ac3928164a7e464aab1c9b7ec77f9adc7
Angga Exca P.S <excaangga@gmail.com>
2021-05-11 07:13:05 +0700

    Pegawai Model, View, Controller
    soon to be fixed

---
+++ resources/views/pegawai/edit.blade.php
@@ -0,0 +1,61 @@
+@extends('pegawai.layout')
+   
+@section('content')
+    <div class="row">
+        <div class="col-lg-12 margin-tb">
+            <div class="pull-left">
+                <h2>Edit pegawai</h2>
+            </div>
+            <div class="pull-right">
+                <a class="btn btn-primary" href="{{ route('pegawai.index') }}"> Kembali</a>
+            </div>
+        </div>
+    </div>
+   
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> Ada kesalahan input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+  
+    <form action="{{ route('pegawai.update',$pegawai->id) }}" method="POST">
+        @csrf
+        @method('PUT')
+   
+         <div class="row">
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Nama:</strong>
+                    <input type="text" name="nama" value="{{ $pegawai->nama }}" class="form-control" placeholder="Nama pegawai">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Kode pegawai:</strong>
+                    <input type="text" name="nama" value="{{ $pegawai->kode_pegawai }}" class="form-control" placeholder="Kode pegawai">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Alamat:</strong>
+                    <textarea class="form-control" style="height:150px" name="alamat" placeholder="Alamat pegawai">{{ $pegawai->alamat }}</textarea>
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12">
+                <div class="form-group">
+                    <strong>Status keaktifan:</strong>
+                    <input type="text" name="is_active" value="{{ $pegawai->is_active }}" class="form-control" placeholder="Status keaktifan pegawai">
+                </div>
+            </div>
+            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+              <button type="submit" class="btn btn-primary">Submit</button>
+            </div>
+        </div>
+   
+    </form>
+@endsection
