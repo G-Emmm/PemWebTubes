8a2640195eefe59c7fa6bc3dbe7ff089f1e9dfe9
FarhanSungkar <72081677+FarhanSungkar@users.noreply.github.com>
2021-06-17 21:58:46 +0700

    Update welcome.blade.php
    Perubahan tampilan

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,20 +12,7 @@
         <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/cover/">
         <!-- Styles -->
         <style>
-            /*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */ .bd-placeholder-img {
-        font-size: 1.125rem;
-        text-anchor: middle;
-        -webkit-user-select: none;
-        -moz-user-select: none;
-        user-select: none;
-      }
-
-      @media (min-width: 768px) {
-        .bd-placeholder-img-lg {
-          font-size: 3.5rem;
-        }
-      }
-      /*
+       /*
  * Globals
  */
 
@@ -80,47 +67,29 @@ body {
 }
 
         </style>
-
-        <style>
-            body {
-                font-family: 'Nunito', sans-serif;
-            }
-            img{
-                width: 1000px;
-                height: 500px;
-            }
-        </style>
     </head>
-    <body class="d-flex h-auto text-center text-white bg-dark">
+    <body class="d-flex h-100 text-center text-white bg-dark">
     
-<div class="container d-flex w-auto h-auto p-3 mx-auto flex-column">
-  <header class="mb-auto">
-    @if (Route::has('login'))
-                <div>
-            <h3 class="float-md-start mb-0">Sistem Kinerja Pegawai</h3>
-            <nav class="nav nav-masthead justify-content-center float-md-end">
-                    @auth
-                        <a href="{{ url('/home') }}" class="nav-link">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="nav-link">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="nav-link">Register</a>
-                        @endif
-                    @endauth
-            </nav>
-                </div>
-            @endif
-  </header>
-
-  <main class="px-3">
-    <img src="https://static.wixstatic.com/media/358a19_0022af5940c94a159571e83159c9a760.jpg/v1/fill/w_392,h_218,al_c,q_80,usm_0.66_1.00_0.01/358a19_0022af5940c94a159571e83159c9a760.jpg">
+    <div class="cover-container d-flex w-100 h-100 p-3 mx-auto flex-column">
    
-  </main>
-  
 
-  <footer class="mt-auto text-white-50">
-    <p>Cover template for <a href="https://getbootstrap.com/" class="text-white">Bootstrap</a>, by <a href="https://twitter.com/mdo" class="text-white">@mdo</a>.</p>
+  <main class="px-3 " style="margin-top:200px;">
+    <h1>Sistem Kinerja Pegawai</h1>
+    <p class="lead">Selamat Datang</p>
+    @auth
+    <p class="lead">
+      <a href="{{ url('/home') }}" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Home</a>
+    </p>
+    @else
+    <p class="lead">
+      <a href="{{ route('login') }}" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Login</a>
+  &nbsp Atau &nbsp
+    <a href="{{ route('register') }}" class="btn btn-lg btn-secondary fw-bold border-white bg-white">Sign up</a>
+    </p>
+    @endauth
+  </main>
+  <footer class="text-white-50" style="  margin-top:185px;">
+    <p>Kelompok 1</p>
   </footer>
 </div>
 
